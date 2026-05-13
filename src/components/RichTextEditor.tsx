81867f2ac3657b9fc35c7a2708368845e9f67dff
Game Wiki Admin <admin@game-wiki.com>
2026-05-13 13:32:00 +0800

    fix: TextStyle 改为命名导入

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -5,7 +5,7 @@ import StarterKit from '@tiptap/starter-kit'
 import Link from '@tiptap/extension-link'
 import TextAlign from '@tiptap/extension-text-align'
 import Underline from '@tiptap/extension-underline'
-import TextStyle from '@tiptap/extension-text-style'
+import { TextStyle } from '@tiptap/extension-text-style'
 import Color from '@tiptap/extension-color'
 import { useEffect, useRef, useState } from 'react'
 
